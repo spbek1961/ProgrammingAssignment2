77501c1eb8e4121d576d050ca8635d90298e9606
spbek1961 <spbek1961@gmail.com>
2014-12-04 15:19:39 +0000

    "Assignmet 2. Working functions. v.1"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions of project 2. Week 2. R Programming Coursera course.
+## Two functions that calculate and cache the inverse of a matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix(). Stores a matrix and caches its inverse.
+## It has methods to control that the cached inverse corresponds with
+## the stored matrix
+makeCacheMatrix <- function(x = matrix()) 
+{
+      invers <- NULL   
+      newmat <- TRUE   #TRUE if the matrix has changed
+      
+      set <- function(mat = matrix())   #store a new matrix and initializes variables
+      {     
+            x <<- mat
+            invers <<- NULL
+            newmat <<- TRUE   
+      }
+      
+      get         <-function()            {x}   #
+      getNewMat   <-function()            {newmat}   
+      setNewMat   <-function(ch)          {newmat<<-ch}
+      setInverse  <-function(inv)         {invers <<- inv}
+      getInverse  <-function()            {invers}
+      list(set=set, get=get, setInverse=setInverse, getInverse=getInverse, 
+           setNewMat=setNewMat, getNewMat=getNewMat)
+      
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(). Computes the inverse of a matrix created by makeCacheMatrix
+## If the inverse has already been calculated and the matrix hasn't changed,
+##cacheSolve() returns the inverse from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+      #Check that the matrix is square. Only square matrices are inversible.
+      ncol(x$get()) != nrow(x$get())
+      if (ncol(x$get()) != nrow(x$get()))   
+      {
+            message("The matrix must have the same number of rows and columns.
+                    Please, pass a square matrix.")
+            return(NULL)
+      }
+      #Check if inverse is already calculated
+      invers <- x$getInverse()
+      if (!is.null(invers) & !x$getNewMat())
+      {
+            message("getting cached data")
+            return(invers)   #if so return cache value
+      }
+      #If not previously calculated, calculate inverse
+      data <- x$get()
+      invers <- solve(data, ...)
+      x$setInverse(invers)
+      x$setNewMat(FALSE)
+      invers
 }
